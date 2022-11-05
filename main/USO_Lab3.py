ca458db9bba59d0f6fe2d728185b69ce0eeb2330
Szymon <sz.zalas@gmail.com>
2022-11-05 03:40:20 +0100

    Changed functions layout Added 'gibberish' function - temp. function

--- main/USO_Lab3.py
+++ main/USO_Lab3.py
@@ -3,13 +3,16 @@ import scipy.signal as sig
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-def zad2():
-    a=np.array([[0,2,-3],[1,0,-2],[0,1,0]])
-    b=np.array([[1],[0],[0]])
+def gibberish():
+    a = np.array([[0, 2, -3], [1, 0, -2], [0, 1, 0]])
+    b = np.array([[1], [0], [0]])
     # macierz*wektor => np.dot(m,w)
     print(b)
-    print(np.dot(a,b))
-    print(np.dot(np.dot(a,a),b))
+    print(np.dot(a, b))
+    print(np.dot(np.dot(a, a), b))
+
+def zad2():
+    pass
 
 if __name__ == '__main__':
     zad2()
